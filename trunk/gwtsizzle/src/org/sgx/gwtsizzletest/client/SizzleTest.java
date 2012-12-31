68e72bb6f96ba2f946e082a991615699b160137c
sebastigurin <sebastigurin@aa5d9e18-1a43-07f5-3f41-1492505a3b18>
2012-12-31 21:38:35 +0000

    changed to static methods

--- trunk/gwtsizzle/src/org/sgx/gwtsizzletest/client/SizzleTest.java
+++ trunk/gwtsizzle/src/org/sgx/gwtsizzletest/client/SizzleTest.java
@@ -3,12 +3,25 @@ package org.sgx.gwtsizzletest.client;
 import org.sgx.gwtsizzle.client.Sizzle;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JsArray;
+import com.google.gwt.dom.client.Element;
 
 public class SizzleTest implements EntryPoint {
 
-	@Override
-	public void onModuleLoad() {
-		System.out.println(Sizzle.get().sizzleArray("div p").length());
-	}
+@Override
+public void onModuleLoad() {
+	
+//Sizzle sizzle = Sizzle.get(); 
+
+//ala JavaScript
+JsArray<Element> result = Sizzle.sizzleArray("div p"); 
+for (int i = 0; i < result.length(); i++) 
+	System.out.println(result.get(i).getTagName());	
+
+//ala Java
+for(Element e : Sizzle.sizzleCol("div p")) 
+	System.out.println(e.getTagName());
+	
+}
 
 }
